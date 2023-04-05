f7b97474bfed2c32fb0ca7bdc6169032733fa0b0
shaikrasheed99 <shaikrasheed3014@gmail.com>
2023-04-05 22:16:44 +0530

    Add. Home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import "./App.css"
+import Home from "./components/home/Home"
+import Navbar from "./components/navbar/Navbar"
 
 function App() {
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <Navbar />
+      <Home />
     </div>
   )
 }
