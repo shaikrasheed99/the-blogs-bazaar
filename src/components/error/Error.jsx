3119fb4e495e636b77ca3c959790ca2187f9e5c2
shaikrasheed99 <shaikrasheed3014@gmail.com>
2023-04-07 00:33:20 +0530

    Add. Error component

---
+++ src/components/error/Error.jsx
@@ -0,0 +1,10 @@
+import "./Error.css"
+const Error = () => {
+    return (
+        <div className="error">
+            <h1>Oops! Got an error while fetching blogs!!</h1>
+        </div>
+    );
+}
+
+export default Error;
