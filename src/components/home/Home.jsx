3c8a9c7e682227933ef92f412a0b262e13f9f099
shaikrasheed99 <shaikrasheed3014@gmail.com>
2023-04-06 19:37:18 +0530

    Add. CSS styles to Home component

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
+import BlogList from "../blogList/BlogList";
+import "./Home.css"
+
 const Home = () => {
+    const [blogs, setBlogs] = useState([
+        {title: "Title A", author: "Author A", id: 1},
+        {title: "Title B", author: "Author B", id: 2},
+        {title: "Title C", author: "Author c", id: 3},
+    ]);
+
     return (
         <div className="home">
-            <h1>Homepage</h1>
+            <h1>List of blogs!</h1>
+            <BlogList blogs={blogs} />
         </div>
     );
 }
