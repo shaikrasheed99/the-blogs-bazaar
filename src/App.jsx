b0741a03356ac10c8ec2c78fb8d3525929fa6527
shaikrasheed99 <shaikrasheed3014@gmail.com>
2023-04-08 17:57:03 +0530

    Add. Routes in root component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
+import { Route, BrowserRouter as Router, Routes} from "react-router-dom"
 import "./App.css"
 import Home from "./components/home/Home"
 import Navbar from "./components/navbar/Navbar"
+import CreateBlog from "./components/createBlog/CreateBlog"
+import Blog from "./components/blogs/Blog"
 
 function App() {
   return (
-    <div className="container">
-      <Navbar />
-      <Home />
-    </div>
+    <Router>
+      <div className="container">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create" element={<CreateBlog />} />
+          <Route path="/blogs/:id" element={<Blog />} />
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
-export default App
+export default App;
