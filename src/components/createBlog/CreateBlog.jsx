d09c802d405e65caa9b4272d9e39736f3e443f74
shaikrasheed99 <shaikrasheed3014@gmail.com>
2023-04-09 12:49:08 +0530

    Add. CSS styles to CreateBlog component

--- src/components/createBlog/CreateBlog.jsx
+++ src/components/createBlog/CreateBlog.jsx
@@ -1,7 +1,66 @@
+import { useState } from "react";
+import "./CreateBlog.css";
+
 const CreateBlog = () => {
+    const [title, setTitle] = useState("");
+    const [author, setAuthor] = useState("");
+    const [body, setBody] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const blog = {title, author, body};
+        const url = "http://localhost:3000/blogs";
+
+        setIsLoading(true);
+
+        fetch(url, {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(blog)
+        }).then(() => {
+            console.log("Blog has added!!")
+            setIsLoading(false);
+        }).catch((error) => {
+            console.log(error)
+        })   
+    }
+
     return (
         <div className="create-blog">
             <h1>Add new blog!!</h1>
+
+            <form onSubmit={handleSubmit}>
+                <label>Blog Title:</label>
+                <input 
+                    type="text"
+                    required
+                    value={title}
+                    onChange={(event) => setTitle(event.target.value)}
+                />
+
+                <label>Blog Author:</label>
+                <input 
+                    type="text"
+                    required
+                    value={author}
+                    onChange={(event) => setAuthor(event.target.value)}
+                />
+
+                <label>Blog Body:</label>
+                <textarea 
+                    type="text"
+                    required
+                    value={body}
+                    onChange={(event) => setBody(event.target.value)}
+                />
+
+                <div className="btn">
+                    {!isLoading && <button className="active">Submit</button>}
+                    {isLoading && <button className="diactive">Submit</button>}
+                </div>
+            </form>
         </div>
     );
 }
